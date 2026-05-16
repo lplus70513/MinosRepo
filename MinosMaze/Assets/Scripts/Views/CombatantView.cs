2a73461a57e08e7980d033f30929f239b7ff0d76
lplus7 <2112926232@qq.com>
2026-05-16 17:09:37 +0800

    feat : 添加防御效果，修改伤害计算方式

--- MinosMaze/Assets/Scripts/Views/CombatantView.cs
+++ MinosMaze/Assets/Scripts/Views/CombatantView.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using DG.Tweening;
+using DG.Tweening; // 确保引用了 DOTween
 
 public class CombatantView : MonoBehaviour
 {
     [SerializeField] private TMP_Text healthText;
     [SerializeField] private SpriteRenderer spriteRenderer;
 
-    public int MaxHealth { get; private set; }
+    [SerializeField] private StatusEffectsUI statusEffectsUI;
 
+    public int MaxHealth { get; private set; }
     public int CurrentHealth { get; private set; }
+
+    private Dictionary<StatusEffectType, int> statusEffects = new();
 
     protected void SetupBase(int health, Sprite image)
     {
@@ -27,12 +30,68 @@ public class CombatantView : MonoBehaviour
 
     public void Damage(int damageAmount)
     {
-        CurrentHealth -= damageAmount;
-        if(CurrentHealth < 0)
+        int remainingDamage = damageAmount;
+        int currentArmor = GetStatusEffectStacks(StatusEffectType.ARMOR);
+        if (currentArmor > 0)
         {
-            CurrentHealth = 0;
+            if(currentArmor >= damageAmount)
+            {
+                RemoveStatusEffect(StatusEffectType.ARMOR, remainingDamage);
+                remainingDamage = 0;
+            }
+            else if(currentArmor < damageAmount)
+            {
+                RemoveStatusEffect(StatusEffectType.ARMOR, currentArmor);
+                remainingDamage -= currentArmor;
+            }
         }
+
+        if (remainingDamage > 0)
+        {
+            CurrentHealth -= remainingDamage;
+            if (CurrentHealth < 0)
+            {
+                CurrentHealth = 0;
+            }
+
+        }
+
         transform.DOShakePosition(0.2f, 0.5f);
-        UpdateHealthText() ;
+        UpdateHealthText();
+    }
+
+    public void AddStatusEffect(StatusEffectType type, int stackCount)
+    {
+        if (statusEffects.ContainsKey(type))
+        {
+            statusEffects[type] += stackCount;
+        }
+        else
+        {
+            statusEffects.Add(type, stackCount);
+        }
+        // 调用方法
+        statusEffectsUI.UpdateStatusEffectUI(type, GetStatusEffectStacks(type));
+    }
+
+    private void RemoveStatusEffect(StatusEffectType type, int stackCount)
+    {
+        if (statusEffects.ContainsKey(type))
+        {
+            statusEffects[type] -= stackCount;
+            if (statusEffects[type] <= 0)
+            {
+                statusEffects.Remove(type);
+            }
+        }
+
+        // 修改点 2: 修复拼写错误 GetStstusEffectStakes -> GetStatusEffectStacks
+        statusEffectsUI.UpdateStatusEffectUI(type, GetStatusEffectStacks(type));
+    }
+
+    public int GetStatusEffectStacks(StatusEffectType type)
+    {
+        if (statusEffects.ContainsKey(type)) return statusEffects[type];
+        else return 0;
     }
 }
