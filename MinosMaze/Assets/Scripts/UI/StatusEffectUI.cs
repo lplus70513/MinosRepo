2de94adfe6db778a08ffe69be0b53f7ef269bc2b
lplus7 <2112926232@qq.com>
2026-06-15 15:35:07 +0800

    feat ： 添加了神秘效果

--- MinosMaze/Assets/Scripts/UI/StatusEffectUI.cs
+++ MinosMaze/Assets/Scripts/UI/StatusEffectUI.cs
@@ -10,6 +10,32 @@ public class StatusEffectUI : MonoBehaviour
 
     [SerializeField] private TMP_Text stackCountText;
 
+    [SerializeField] private Image badgeBackground;
+
+    private void Awake()
+    {
+        var textRect = stackCountText.rectTransform;
+        textRect.anchorMin = new Vector2(1f, 0f);
+        textRect.anchorMax = new Vector2(1f, 0f);
+        textRect.pivot = new Vector2(0.5f, 0.5f);
+        textRect.anchoredPosition = new Vector2(-12f, 12f);
+        textRect.sizeDelta = new Vector2(20f, 20f);
+        stackCountText.fontSize = 14;
+        stackCountText.fontStyle = FontStyles.Bold;
+        stackCountText.color = Color.white;
+
+        if (badgeBackground != null)
+        {
+            var badgeRect = badgeBackground.rectTransform;
+            badgeRect.anchorMin = new Vector2(1f, 0f);
+            badgeRect.anchorMax = new Vector2(1f, 0f);
+            badgeRect.pivot = new Vector2(0.5f, 0.5f);
+            badgeRect.anchoredPosition = new Vector2(-12f, 12f);
+            badgeRect.sizeDelta = new Vector2(24f, 24f);
+            badgeBackground.transform.SetAsFirstSibling();
+        }
+    }
+
     public void Set(Sprite sprite, int stackCount)
     {
         image.sprite = sprite;
--- MinosMaze/Assets/Scripts/UI/StatusEffectsUI.cs
+++ MinosMaze/Assets/Scripts/UI/StatusEffectsUI.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class StatusEffectsUI : MonoBehaviour
 {
     [SerializeField] private StatusEffectUI statusEffectUIPrefab;
@@ -11,30 +15,106 @@ public class StatusEffectsUI : MonoBehaviour
     [SerializeField] private Sprite fortifySprite, fragileSprite, agileSprite, slowSprite;
     [SerializeField] private Sprite chainLightningSprite, rootSprite, stunSprite;
 
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private float fadeDuration = 0.3f;
+
     private Dictionary<StatusEffectType, StatusEffectUI> statusEffectUIs = new();
+
+    private static Material uiAlwaysVisibleMaterial;
+    private static Shader tmpAlwaysVisibleShader;
+
+    private void Awake()
+    {
+        Canvas parentCanvas = GetComponentInParent<Canvas>();
+        if (parentCanvas != null)
+        {
+            parentCanvas.overrideSorting = true;
+            parentCanvas.sortingOrder = 100;
+        }
+
+        if (canvasGroup == null)
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+            if (canvasGroup == null)
+                canvasGroup = gameObject.AddComponent<CanvasGroup>();
+        }
+        canvasGroup.alpha = 0f;
+
+        if (uiAlwaysVisibleMaterial == null)
+        {
+            Shader shader = Shader.Find("Custom/UIAlwaysVisible");
+            if (shader != null)
+                uiAlwaysVisibleMaterial = new Material(shader);
+        }
+
+        if (tmpAlwaysVisibleShader == null)
+            tmpAlwaysVisibleShader = Shader.Find("Custom/TMPAlwaysVisible");
+    }
+
+    private void ApplyAlwaysVisibleMaterial(StatusEffectUI ui)
+    {
+        if (uiAlwaysVisibleMaterial != null)
+        {
+            var images = ui.GetComponentsInChildren<Image>();
+            foreach (var img in images)
+                img.material = uiAlwaysVisibleMaterial;
+        }
+
+        if (tmpAlwaysVisibleShader != null)
+        {
+            var texts = ui.GetComponentsInChildren<TMP_Text>();
+            foreach (var tmp in texts)
+            {
+                var fontMat = new Material(tmp.fontMaterial);
+                fontMat.shader = tmpAlwaysVisibleShader;
+                tmp.fontMaterial = fontMat;
+            }
+        }
+    }
 
     public void UpdateStatusEffectUI(StatusEffectType statusEffectType, int stackCount)
     {
+        bool wasEmpty = statusEffectUIs.Count == 0;
+
         if (stackCount == 0)
         {
             if (statusEffectUIs.ContainsKey(statusEffectType))
             {
-                // �޸� 1������Ӧ���� StatusEffectUI
                 StatusEffectUI statusEffectUI = statusEffectUIs[statusEffectType];
                 statusEffectUIs.Remove(statusEffectType);
-                Destroy(statusEffectUI.gameObject);
+
+                if (statusEffectUIs.Count == 0)
+                {
+                    canvasGroup.DOKill();
+                    canvasGroup.DOFade(0f, fadeDuration).OnComplete(() =>
+                    {
+                        if (statusEffectUI != null)
+                            Destroy(statusEffectUI.gameObject);
+                    });
+                }
+                else
+                {
+                    Destroy(statusEffectUI.gameObject);
+                }
             }
         }
         else
         {
             if (!statusEffectUIs.ContainsKey(statusEffectType))
             {
-                // �޸� 2������Ӧ���� StatusEffectUI
                 StatusEffectUI statusEffectUI = Instantiate(statusEffectUIPrefab, transform);
+                ApplyAlwaysVisibleMaterial(statusEffectUI);
                 statusEffectUIs.Add(statusEffectType, statusEffectUI);
             }
             Sprite sprite = GetSpriteByType(statusEffectType);
             statusEffectUIs[statusEffectType].Set(sprite, stackCount);
+
+            if (wasEmpty)
+            {
+                canvasGroup.alpha = 0f;
+                canvasGroup.DOKill();
+                canvasGroup.DOFade(1f, fadeDuration);
+            }
         }
     }
 
